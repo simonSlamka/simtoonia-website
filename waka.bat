e01cae57257a033d57699bdfae9ff2df553a37e9
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2022-03-05 20:36:17 +0100

    started work and got a coming soon page done;

---
+++ waka.bat
@@ -0,0 +1,104 @@
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
+C:\Users\Simtoon\.wakatime\wakatime-cli\wakatime-cli-windows-amd64.exe wakatime-cli.exe --write --category designing --project simtoonia --entity-type "app" --entity https://simtoon.tech/ --language html
+timeout /t 60 /nobreak
